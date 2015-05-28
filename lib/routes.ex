8179d50cd8d457fa5c06f3df818431ad84c70625
Riza Fahmi <rizafahmi@gmail.com>
2015-05-28 11:42:03 +0700

    Gabungin all routes

--- lib/routes.ex
+++ lib/routes.ex
@@ -1,10 +1,14 @@
 defmodule TrotIcip.Router do
   use Trot.Router
+  use Trot.Template
+
+  get "/html" do
+    render_template("index.html.eex", [])
+  end
 
   get "/" do
     "Hey, you're in!"
   end
 
-  import_routes TrotIcip.Router.Templates
   import_routes Trot.NotFound
 end
--- lib/routes_templates.ex
+++
@@ -1,8 +0,0 @@
-defmodule TrotIcip.Router.Templates do
-  use Trot.Router
-  use Trot.Template
-
-  get "/html" do
-    render_template("index.html.eex", [])
-  end
-end
